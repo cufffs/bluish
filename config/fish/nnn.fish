477b69547317bbd0ed7af5ff894f685808bd24fb
cal <14177650+cufffs@users.noreply.github.com>
2023-01-24 22:03:42 +1100

    adding some stuff

---
+++ config/fish/nnn.fish
@@ -0,0 +1,3 @@
+export NNN_PLUG='p:preview-tui;v:imgview;j:autojump;c:chksum;f:fzcd;d:dragdrop;r:renamer;s:suedit;x:togglex'
+export NNN_FCOLORS='14E2E62dc300000000000000'
+set --export NNN_FIFO "/tmp/nnn.fifo"
---
+++ config/waybar/screen-rec.sh
@@ -0,0 +1,14 @@
+# $HOME/scripts/toggle-screen-recording.sh
+# ------------------------------------------
+
+#!/bin/bash 
+pid=`pgrep wf-recorder`
+status=$?
+
+if [ $status != 0 ] 
+then 
+  wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
+else 
+  pkill --signal SIGINT wf-recorder
+fi;
+
